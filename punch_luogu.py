b9418594764cea05458d27f57c6dedd92a864fc2
Zhong Jia Xuan <134782384+zjx-kimi@users.noreply.github.com>
2024-10-03 12:08:24 +0800

    Update punch_luogu.py

--- punch_luogu.py
+++ punch_luogu.py
@@ -1,5 +1,5 @@
 import requests
-import sys
+import os
 
 def makeHead(cookie):
     temp = {
@@ -17,11 +17,14 @@ def makeHead(cookie):
     return temp
 
 def punch(cookie):
-    return requests.get('https://www.luogu.com.cn/index/ajax_punch', headers=makeHead(cookie)).text
+    response = requests.get('https://www.luogu.com.cn/index/ajax_punch', headers=makeHead(cookie))
+    # 强制将响应编码设置为 UTF-8
+    response.encoding = 'utf-8'
+    return response.text
 
 if __name__ == "__main__":
-    uid = sys.argv[1]  # 从命令行获取用户的 uid
-    client_id = sys.argv[2]  # 从命令行获取用户的 client_id
+    uid = os.getenv('LUOGU_UID')  # 从环境变量中获取 uid
+    client_id = os.getenv('LUOGU_CLIENT_ID')  # 从环境变量中获取 client_id
     cookie = "__client_id=" + client_id + ";_uid=" + uid + ";"  # 构造 Cookie
     result = punch(cookie)  # 发起签到请求
     print(result)  # 输出签到结果
